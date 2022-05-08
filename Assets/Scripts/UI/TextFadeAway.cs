2f1aa240f2a1a970d3ee07a08c03d5ad3fa02bf4
Patrik Horný <patulda111@gmail.com>
2022-05-08 17:47:10 +0200

    Intro text

---
+++ Assets/Scripts/UI/TextFadeAway.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextFadeAway : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    void Start()
+    {
+
+        InvokeRepeating("FadeText", 4.0f, 0f);
+        InvokeRepeating("Destroy", 6.1f, 0f);
+
+    }
+
+    void FadeText()
+    {
+        text.CrossFadeAlpha(0,2f,false);
+       
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    
+}
