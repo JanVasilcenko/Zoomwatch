53048a45c9dd9f92f4af301c05b8278adb79a233
JanVasilcenko <jan.vasilcenko@gnj.cz>
2022-05-14 08:08:47 +0200

    Random_Zombie_Skin

---
+++ Assets/Scripts/RandomZombieSkin.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomZombieSkin : MonoBehaviour {
+    public GameObject [] allSkins;
+
+    void Start() {
+        ActivateRandomSkin();
+    }
+
+    private void DeactivateAll() {
+        foreach (GameObject skin in allSkins) {
+            skin.SetActive(false);
+        }
+    }
+
+    private void ActivateRandomSkin() {
+        DeactivateAll();
+        allSkins [CommonUtils.RandomBetweenTwoIntegers(0, allSkins.Length)].SetActive(true);
+    }
+}
